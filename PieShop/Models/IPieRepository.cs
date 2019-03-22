8118e10ec9141b6516281a7db57cde78222f794c
Muhammad Amal MY <MAmal@kerrylogistics.com>
2019-03-22 11:19:09 +0800

    add IPieRepos

--- PieShop/Models/IPieRepository.cs
+++ PieShop/Models/IPieRepository.cs
@@ -1,11 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace PieShop.Models
 {
-    interface IPieRepository
+    public interface IPieRepository
     {
+        IEnumerable<Pie> GetAllPies();
+        Pie GetPieById(int pieId);
     }
 }
