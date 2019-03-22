6f19ed052b75ca18822b6add0b1f929a19f3a4bb
Muhammad Amal MY <MAmal@kerrylogistics.com>
2019-03-22 11:27:58 +0800

    add HomeController and constructor with DI

---
+++ PieShop/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PieShop.Models;
+
+namespace PieShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
