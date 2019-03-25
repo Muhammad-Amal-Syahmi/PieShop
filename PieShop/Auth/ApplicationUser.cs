b9418a184667ca9e0276ab95b049621f8ef8da2b
Muhammad Amal MY <MAmal@kerrylogistics.com>
2019-03-25 17:16:13 +0800

    add AppUser class for extending IdentityUser

---
+++ PieShop/Auth/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace PieShop.Auth
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public DateTime BirthDate { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}
