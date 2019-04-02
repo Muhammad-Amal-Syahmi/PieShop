7c96daab6cc20e6be643b3288e6a050dd63a392f
Muhammad Amal MY <MAmal@kerrylogistics.com>
2019-04-02 12:03:18 +0800

    using dapper instead of LINQ in PieRepo

--- PieShop/Repositories/PieRepository.cs
+++ PieShop/Repositories/PieRepository.cs
@@ -56,7 +56,6 @@ namespace PieShop.Models
             {
                 var pieList = new List<Pie>();
 
-                //Dapper codes
                 var query = "SELECT * FROM Pie WHERE  is_pie_of_the_week";
 
                 using (var con = new NpgsqlConnection(_configuration.GetConnectionString("DefaultConnection")))
@@ -64,42 +63,103 @@ namespace PieShop.Models
                     pieList = con.Query<Pie>(query).ToList();
                 }
                 return pieList;
+
+                //LINQ
                 //return _dbContext.Pie.Where(p => p.IsPieOfTheWeek);
             }
         }
 
         public void CreatePie(Pie pie)
         {
-            var query = "INSERT INTO Pie VALUES (" +
-                "\"" + pie.name + "\", " +
-                "\"" + pie.short_description + "\", " +
-                "\"" + pie.long_description + "\", " +
-                pie.price + ", " +
-                "\"" + pie.image_url + "\", " +
-                "\"" + pie.image_thumbnail_url + "\", " +
-                pie.is_in_stock + ", " +
-                pie.is_pie_of_the_week + ", " +
-                pie.category_id + ", " +
-                pie.pie_id + ")";
+            var query = "Insert into Pie ( name, " +
+                "short_description, " +
+                "long_description, " +
+                "price, " +
+                "image_url, " +
+                "image_thumbnail_url, " +
+                "is_in_stock, " +
+                "is_pie_of_the_week, " +
+                "category_id) " +
+                "values (@name, " +
+                "@short_description, " +
+                "@long_description, " +
+                "@price, " +
+                "@image_url, " +
+                "@image_thumbnail_url, " +
+                "@is_in_stock, " +
+                "@is_pie_of_the_week, " +
+                "@category_id)";
 
             using (var con = new NpgsqlConnection(_configuration.GetConnectionString("DefaultConnection")))
             {
-                con.Execute(query);
+                con.Execute(query,
+                    new
+                    {
+                        pie.name,
+                        pie.short_description,
+                        pie.long_description,
+                        pie.price,
+                        pie.image_url,
+                        pie.image_thumbnail_url,
+                        pie.is_in_stock,
+                        pie.is_pie_of_the_week,
+                        pie.category_id
+                    });
             }
+
+            //LINQ
             //_dbContext.Pie.Add(pie);
             //_dbContext.SaveChanges();
         }
 
         public void UpdatePie(Pie pie)
         {
-            _dbContext.Pie.Update(pie);
-            _dbContext.SaveChanges();
+            var query = "UPDATE Pie SET name= @name, " +
+                "short_description= @short_description, " +
+                "long_description= @long_description, " +
+                "price= @price, " +
+                "image_url= @image_url, " +
+                "image_thumbnail_url= @image_thumbnail_url, " +
+                "is_in_stock= @is_in_stock, " +
+                "is_pie_of_the_week= @is_pie_of_the_week, " +
+                "category_id= @category_id " +
+                "WHERE pie_id = @pie_id";
+
+            using (var con = new NpgsqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                con.Execute(query,
+                    new
+                    {
+                        pie.name,
+                        pie.short_description,
+                        pie.long_description,
+                        pie.price,
+                        pie.image_url,
+                        pie.image_thumbnail_url,
+                        pie.is_in_stock,
+                        pie.is_pie_of_the_week,
+                        pie.category_id,
+                        pie.pie_id
+                    });
+            }
+
+            //LINQ
+            //_dbContext.Pie.Update(pie);
+            //_dbContext.SaveChanges();
         }
 
         public void DeletePie(Pie pie)
         {
-            _dbContext.Pie.Remove(pie);
-            _dbContext.SaveChanges();
+            var query = "DELETE FROM  Pie WHERE pie_id = @pie_id";
+
+            using (var con = new NpgsqlConnection(_configuration.GetConnectionString("DefaultConnection")))
+            {
+                con.Execute(query, new { pie.pie_id });
+            }
+
+            //LINQ
+            //_dbContext.Pie.Remove(pie);
+            //_dbContext.SaveChanges();
         }
     }
 }
