099db05d522390573766ddd437c00378712a1dbb
Muhammad Amal MY <MAmal@kerrylogistics.com>
2019-04-04 10:20:59 +0800

    add PieController and Index views, add pie tab. rename PieOfTheWeekSummary to PieCard

---
+++ PieShop/Controllers/PieController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PieShop.Models;
+
+namespace PieShop.Controllers
+{
+    public class PieController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public PieController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+
+        public ViewResult Index()
+        {
+            var allPies = _pieRepository.GetAllPie();
+
+            return View(allPies);
+        }
+
+        public IActionResult Details(int id)
+        {
+            var pie = _pieRepository.GetPieById(id);
+            if (pie == null)
+            {
+                return NotFound();
+            }
+
+            return View(pie);
+        }
+    }
+}
